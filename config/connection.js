c7b66f6e385afbdc88b6239c54b39bc7148ba9e1
vmultani <vmultani@blueyonder.co.uk>
2020-01-11 14:00:35 +0000

    connection.js file ammended

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Airbus_97",
-  database: "employeeDB"
+  database: "employeedb"
 });
 
 connection.connect(err => {
